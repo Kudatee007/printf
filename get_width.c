e764f3ac75620bcb0356582b66541121d4e7f53e
Fodayjihared <106770599+Fodayjihared@users.noreply.github.com>
2023-09-18 12:36:38 +0000

    Update get_width.c
    Updated

--- get_width.c
+++ get_width.c
@@ -1,36 +1,36 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * is_digit - Check if a character is a digit.
+ * @c: The character to check.
+ * Return: true if it's a digit, false otherwise.
+ */
+bool is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * get_width - Calculates the width
+ * @format: Formatted string.
+ * @i: pointer argument
+ * @list: list of argumens.
  *
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
 {
-	int curr_i;
 	int width = 0;
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			width *= 10;
-			width += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			width = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (width);
+	while (is_digit(format[*i]))
+    {
+	    width *= 10;
+	    width += format[(*i)++] - '0';
+    }
+    if (format[*i] == '*')
+    {
+	    (*i)++;
+	    width = va_arg(list, int);
+    }
+    return (width);
 }
